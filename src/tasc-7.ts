0a83b43df4e5d0da74c49bd0c88dcdb4e9d41490
Yaroslav13-13 <frilanserwork13@gmail.com>
2025-09-09 23:21:46 +0300

    blended-react

--- src/tasc-7.ts
+++ src/tasc-7.ts
@@ -4,9 +4,17 @@
 //* Створіть інтерфейс User, який описує структуру об’єкта з іменем і віком.
 //* Типізуйте змінну users.
 //* Додайте ще одного користувача до масиву, дотримуючись структури.
-//*     Переконайтеся, що TypeScript не дозволяє додати об’єкт без обов’язкових полів(name, age).
+//* Переконайтеся, що TypeScript не дозволяє додати об’єкт без обов’язкових полів(name, age).
 
-const users = [
+interface User {
+  name: string;
+  age: number;
+}
+
+const users: User[] = [
   { name: "Alice", age: 30 },
   { name: "Bob", age: 25 },
 ];
+
+users.push({ name: "Charlie", age: 28 });
+console.log(users);
